91d941b9174cec219459d0ee5034a70105eb5ae2
Brian Simpson <brian@inhand.co.uk>
2020-03-19 15:52:48 +0000

    Added back dvsa/eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     "jest": true
   },
   "extends": [
+    // "@dvsa/eslint-config-ts"
     "airbnb-typescript/base",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
